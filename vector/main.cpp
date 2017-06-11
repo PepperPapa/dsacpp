0fa688aa162c7c75d09f07541d743dc306bfbb25
pepperpapa <zhongxin2506@outlook.com>
2017-06-11 22:41:47 +0800

    扩展Vector模板类接口： // Vector类声明 template <class T> class Vector { public:  Vector(int capacity = DEFAULT_CAPACITY, int size = 0, T v = 0);  ~Vector ();  void show(); // only for test  Rank size(); // 报告向量当前的规模（元素总数）  Rank insert(Rank r, T e); // e作为秩为r元素插入，原后继依次后移  T get(Rank r); // 获取秩为r的元素  Rank put(Rank r, T e); // 用e替换秩为r元素的数值  T remove(Rank r); // 删除秩为r的元素，返回该元素原值  int disordered(); // 判断所有元素是否已按非降序排列, 返回逆序相邻元素对的总数  void bubbleSort(Rank lo, Rank hi); // 调整各元素的位置，使之按非降序排列(冒泡排序)  void mergeSort(Rank lo, Rank hi); // 调整各元素的位置，使之按非降序排列(归并排序)
    private:
     Rank _size; // 长度
     int _capacity; // 存储空间的规模
     T* _elem; // 向量数据区
     void expand(); // 空间不足时以2倍现有空间进行扩容
     void swap(Rank r1, Rank r2); // 交换秩为r1、r2的数据
    };

--- vector/main.cpp
+++ vector/main.cpp
@@ -4,7 +4,6 @@
 
 // 依据邓俊辉老师的《数据结构》mooc课程的练习
 #include <iostream>
-
 #include "vector.h"
 
 using namespace std;
@@ -12,12 +11,19 @@ using namespace std;
 int main() {
 	Vector<int> v;
 	cout << "初始向量长度为：" << v.size() << endl;
-	for (int i = 0; i < 100; i++) {
+	for (int i = 0; i < 10; i++) {
 		v.insert(i, i);
 		cout << "向量秩为" << i << "的元素为：" << v.get(i) << endl;
 	}
+	v.put(0, 89);
+	v.put(6, 0);
+	v.put(9, 2);
 	cout << "目前向量的长度为：" << v.size() << endl;
-	
+	v.show();
+	v.mergeSort(0, v.size());
+	v.show();
+
+	// 运行结束后暂停关闭控制台
 	system("pause");
 	return 0;
 }
--- vector/vector.h
+++ vector/vector.h
@@ -1,6 +1,7 @@
 ﻿// Copyright 2017 zhongxin
 // license: BSD
 // author: zhongxin
+
 #include <iostream>
 
 using namespace std;
@@ -14,15 +15,22 @@ class Vector {
 public:
 	Vector(int capacity = DEFAULT_CAPACITY, int size = 0, T v = 0);
 	~Vector ();
+	void show(); // only for test
 	Rank size(); // 报告向量当前的规模（元素总数）
 	Rank insert(Rank r, T e); // e作为秩为r元素插入，原后继依次后移
 	T get(Rank r); // 获取秩为r的元素
+	Rank put(Rank r, T e); // 用e替换秩为r元素的数值
+	T remove(Rank r); // 删除秩为r的元素，返回该元素原值
+	int disordered(); // 判断所有元素是否已按非降序排列, 返回逆序相邻元素对的总数
+	void bubbleSort(Rank lo, Rank hi); // 调整各元素的位置，使之按非降序排列(冒泡排序)
+	void mergeSort(Rank lo, Rank hi); // 调整各元素的位置，使之按非降序排列(归并排序)
 
 private:
 	Rank _size; // 长度
 	int _capacity; // 存储空间的规模
 	T* _elem; // 向量数据区
 	void expand(); // 空间不足时以2倍现有空间进行扩容
+	void swap(Rank r1, Rank r2); // 交换秩为r1、r2的数据
 };
 
 // TIP: 以下类成员函数实现代码和声明必须在同一个头文件内编译才能通过，否则会报错
@@ -41,6 +49,18 @@ Vector<T>::Vector(int capacity = DEFAULT_CAPACITY, int size = 0, T val = 0){
 template <class T>
 Vector<T>::~Vector() {
 	delete [] _elem;
+}
+
+// show method: only for test
+template <class T>
+void Vector<T>::show() {
+	if (_size > 0) {
+		cout << "[";
+		for (int i = 0; i < _size; i++) {
+			cout << _elem[i] << ",";
+		}
+		cout << "]" << endl;
+	}
 }
 
 template <class T>
@@ -84,5 +104,92 @@ void Vector<T>::expand() {
 		}
 
 		delete [] old_elem;
+	}
+}
+
+template <class T>
+Rank Vector<T>::put(Rank r, T e) {
+	if ((r >= 0) && (r < _size)) {
+		_elem[r] = e;
+		return r;
+	}
+	return -1;
+}
+
+template <class T>
+T Vector<T>::remove(Rank r) {
+	if ((r >= 0) && (r < _size)) {
+		for (int i = r; i < (_size - 1); i++) {
+			_elem[i] = _elem[i + 1];
+		}
+		_size -= 1;
+	}
+	return -1;
+}
+
+template <class T>
+int Vector<T>::disordered() {
+	int n = 0; // 记录逆序相邻元素对的个数
+	for (int i = 0; i < (_size - 1); i++) {
+	  if (_elem[i] > _elem[i + 1]) {
+	  	n += 1;
+	  }
+	}
+	return n;
+}
+
+template <class T>
+void Vector<T>::swap(Rank r1, Rank r2) {
+		T temp = _elem[r1];
+		_elem[r1] = _elem[r2];
+		_elem[r2] = temp;
+}
+
+template <class T>
+void Vector<T>::bubbleSort(Rank lo, Rank hi) {
+	// 冒泡排序
+	// 共进行_size轮冒泡
+	for (int i = lo; i < hi; i++) {
+	  // 每一轮检查_size-1次相邻数据是否非升序，如果是则交换数据
+		for (int j = lo; j < (hi - 1); j++) {
+			if (_elem[j] > _elem[j + 1]) {
+				swap(j, j + 1);
+			}
+		}
+	}
+}
+
+template <class T>
+void Vector<T>::mergeSort(Rank lo, Rank hi) {
+	// 归并排序
+	if (lo < hi - 1) {  // lo == hi表示只剩一个字序列，自然已经有序了
+		// 递归分解
+		Rank mid = (lo + hi) / 2;
+		mergeSort(lo, mid);
+		mergeSort(mid, hi);
+		// 合并
+		int i = lo; // 用于跟踪第一子序列位置
+		int j = mid; // 用于跟踪第二个字序列位置
+		T* temp = new T[hi - lo]; // [lo, hi)左开右闭区间，用来临时存放合并的序列
+		int k = 0; // 用于跟踪temp临时序列的位置
+		while (true) {
+			if ((_elem[i] > _elem[j]) && (j < hi)) {
+				temp[k] = _elem[j];
+				j++;
+				k++;
+			} else {
+				temp[k] = _elem[i];
+				i++;
+				k++;
+			}
+			if ((i >= mid) && (j >= hi)) {
+				break;
+			}
+		}
+
+		for (i = lo; i < hi; i++) {
+		  _elem[i] = temp[i - lo];
+		}
+		delete [] temp;
 	}
 }
